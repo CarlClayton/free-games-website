2b78b89c9356e621e79796f0eebf1dc0f244d33b
CarlClayton <carlclayton1984@gmail.com>
2023-04-05 15:17:31 +0100

    fix: update webpack

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin')
 const Dotenv = require('dotenv-webpack')
 
 module.exports = {
-	mode: 'development',
+	mode: 'none',
 	entry: './src/index.tsx',
 	output: {
 		path: path.resolve(__dirname, 'dist'),
